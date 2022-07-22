3b2bf9db8aeb1ed79dd1d055ad050370c705dd38
Luna <dev@lunardiver.com>
2022-07-22 07:35:41 +0200

    Add project files.

---
+++ ItemRecoveryPlus/DialoguePatch.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using HarmonyLib;
+using StardewValley;
+using StardewValley.Menus;
+
+namespace ItemRecoveryPlus
+{
+    [HarmonyPatch]
+    internal static class DialoguePatch
+    {
+        // ReSharper disable once InconsistentNaming
+        public static void Postfix(ref bool __result, string questionAndAnswer)
+        {
+            if(questionAndAnswer is not "adventureGuild_RecoveryPlus")
+                return;
+
+            Game1.activeClickableMenu = new ShopMenu(Game1.player.itemsLostLastDeath.Cast<ISalable>().ToList(), who: "Marlon_Recovery");
+
+            Game1.player.itemsLostLastDeath.Clear();
+
+            __result = true;
+        }
+    }
+}
---
+++ ItemRecoveryPlus/ModEntry.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using HarmonyLib;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Locations;
+using StardewValley.Menus;
+
+namespace ItemRecoveryPlus
+{
+    internal class ModEntry : Mod
+    {
+        /// <inheritdoc />
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Display.MenuChanged += MenuChangedEvent;
+
+            var harmony = new Harmony(ModManifest.UniqueID);
+            harmony.Patch(
+                AccessTools.Method(
+                    typeof(GameLocation), 
+                    nameof(GameLocation.answerDialogueAction)),
+                postfix: new HarmonyMethod(
+                    typeof(DialoguePatch), 
+                    nameof(DialoguePatch.Postfix)));
+        }
+
+        private void MenuChangedEvent(object sender, MenuChangedEventArgs e)
+        {
+            if(Game1.currentLocation is not AdventureGuild)
+                return;
+            
+            if(e.NewMenu is not DialogueBox dialogue)
+                return;
+            
+            const string shopKey = "Shop";
+            const string recovKey = "Recovery";
+            const string leaveKey = "Leave";
+
+            string[] responseKeys = dialogue.responses.Select(res => res.responseKey).ToArray();
+            if(!responseKeys.Contains(shopKey) || !responseKeys.Contains(recovKey) || !responseKeys.Contains(leaveKey))
+                return;
+
+            Response recoveryResponse = dialogue.responses.First(res => res.responseKey.Equals(recovKey));
+
+            dialogue.responses[dialogue.responses.IndexOf(recoveryResponse)] = new Response("RecoveryPlus", recoveryResponse.responseText);
+            return;
+
+            
+        }
+    }
+}
